3a66eaabad12aca8d1dc16099722221f550a781a
Joey Palchak <joey.palchak@gmail.com>
2023-08-14 19:24:16 -0700

    Initiate git repo. Start createDeckOfCards() function with two arrays.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+function createDeckOfCards() {
+
+  // Create two arrays. We have four suits, and 13 ranks per suit.
+  const suits = ['Spades', 'Hearts', 'Diamonds','Clubs'];
+  const ranks = ['Ace', '2', '3', '4','5','6','7','8','9','10','Jack','Queen','King'];
+
+
+
+}
+
+window.addEventListener('load', createDeckOfCards);
